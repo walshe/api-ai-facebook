4c92be1993947c6748ddfd1f8ac8365ad685de72
Emmett Walsh <emmett@boylesoftware.com>
2016-06-30 15:40:53 -0400

    tetsing output

--- src/app.js
+++ src/app.js
@@ -183,7 +183,7 @@ function sendFBMessage(sender, messageData, callback) {
                 "attachment":{
                     "type":"image",
                     "payload":{
-                        "url":"http://bot-mediator.herokuapp.com/UWS/Logo Restaurants/QR Code Coupon/Save!.png"
+                        "url":"http://bot-mediator.herokuapp.com/NY/BK.jpg"
                     }
                 }
             }
