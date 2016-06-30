688d5d142b5f9b40b12fa7308764f011cebb09e7
Emmett Walsh <emmett@boylesoftware.com>
2016-06-30 15:43:10 -0400

    rename folder

--- src/app.js
+++ src/app.js
@@ -183,7 +183,7 @@ function sendFBMessage(sender, messageData, callback) {
                 "attachment":{
                     "type":"image",
                     "payload":{
-                        "url":"http://bot-mediator.herokuapp.com/NY/BK.jpg"
+                        "url":"http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/Save!.png"
                     }
                 }
             }
